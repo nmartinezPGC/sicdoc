09fd40545f95e2b1867e201bf52d07be7a8d344a
nmartinezPGC <nahum.sreci@gmail.com>
2017-08-19 20:30:30 -0600

    NAM | Objeto jsonParser
    Convertimos los arreglos de la Informacion de la BD - Tabla Paises en un Objeto Json

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -21,12 +21,48 @@ class DefaultController extends Controller
     
     /**
      * @Route("/pais", name="pais")
+     * Creacion del Servicio: Lista de Paiese
+     * @author Nahum Martinez <nmartinez.salgado@yahoo.com>
+     * @since 1.0
+     * Funcion: FND00001
      */
     public function paisAction(Request $request)
     {
-        $em = $this->getDoctrine()->getManager(); 
+        //Definimos el Objeto Entitir Manager ($em)
+        $em = $this->getDoctrine()->getManager();
+        //Definicion del Repositorio de la Entidad Paises
         $paises = $em->getRepository('BackendBundle:TblPais')->findAll();
-        var_dump($paises);
-        die();
+        //var_dump($paises);
+        //die();
+        //Convertimos el Objeto Json, llamando la Funcion parserJson(); y le
+        //damos como parametro los Paises ($paises)
+        return $this->parserJson($paises);
     }
+    
+    /**
+     * Transformacion de ls Datos a: Json, con Normalizer
+     * @author Nahum Martinez <nmartinez.salgado@yahoo.com>
+     * @since 1.0
+     * Funcion: FND00001 
+     */
+    public function parserJson($data) {
+        //Instanciamos el Objeto Normalizer
+        $normalizer = array( new \Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer() );
+        //Instanciamos el Objeto Encoder de PHP
+        $encoders = array("json" => new \Symfony\Component\Serializer\Encoder\JsonEncoder());
+        //Instanciamos el Objeto serializer de PHP
+        $serializer = new \Symfony\Component\Serializer\Serializer($normalizer, $encoders);
+        //Definicion del Objeto Json de PHP
+        $json = $serializer->serialize($data, 'json');
+       
+        //Respuesta del Objeto Json a Formato Html
+        $response = new \Symfony\Component\HttpFoundation\Response();
+        $response->setContent($json);
+        $response->headers->set("Content-Type", "application/json");
+        
+        //Retornamos la Respuesta
+        return $response;        
+    }//Fin de Funcion: FND00001
+    
+    
 }
