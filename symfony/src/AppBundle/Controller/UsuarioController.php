dda12a2e0907a679c1d162b63b1c9f68080888e1
Nahum Martinez <nahum.sreci@gmail.com>
2017-08-21 11:13:13 -0600

    NAM | Subida de Imagen - Usuario
    Se agrego el controlador del usuario, la opcion de Subir la Imagen del usuario.

--- symfony/src/AppBundle/Controller/UsuarioController.php
+++ symfony/src/AppBundle/Controller/UsuarioController.php
@@ -150,7 +150,7 @@ class UsuarioController extends Controller{
         return $helpers->parserJson($data);
     }    
     
-    //Funcion para Editar Usuario ***********************************************************
+    //Funcion para Editar Usuario ********************************************************
     //************************************************************************************
     public function editAction(Request $request) {
         //Instanciamos el Servicio Helpers
@@ -299,5 +299,75 @@ class UsuarioController extends Controller{
         //Retorno de la Funcion ************************************************
         return $helpers->parserJson($data);
     } 
+    
+    //Metodo de Subir Imagen *************************************************************
+    //************************************************************************************
+    public function uploadImageAction(Request $request) {
+        //Instanciamos el Servicio Helpers
+        $helpers = $this->get("app.helpers");        
+        //Recoger el Hash
+        //Recogemos el Hash y la Autrizacion del Mismo
+        $hash = $request->get("authorization", null);
+        //Se Chekea el Token
+        $checkToken = $helpers->authCheck($hash);
+        //Evaluamos la Autoriuzacion del Token
+        if($checkToken == true){
+        //Ejecutamos todo el Codigo restante
+        $identity = $helpers->authCheck($hash, true);    
+        $em = $this->getDoctrine()->getManager();
+        //Buscamos el registro por el Id de Usaurio
+        $usuario = $em->getRepository("BackendBundle:TblUsuarios")->findOneBy(
+            array(
+               "idUsuario" => $identity->sub
+            ));
+            //Recoger el Fichero que viene por el POST y lo guardamos el HD
+            $file = $request->files->get("image");
+            //Se verifica que el fichero no venga Null
+            if (!empty($file) && $file != null) {
+                //Obtenemos la extencion del Fichero
+                $ext = $file->guessExtension();
+                //Comprobamos que la Extencion sea Aceptada
+                if ($ext == "jpeg" || $ext == "jpg" || $ext == "png" || $ext == "gif") {                   
+                    // Concatenmos al Nombre del Fichero la Fecha y la Extencion
+                    $file_name = time().".".$ext;
+                    //Movemos el Fichero
+                    $file->move("uploads/users", $file_name);
+
+                    //Seteamos el valor de la Imagen dentro de la Tabla:Tblusuarios+
+                    $usuario->setImagenUsuario($file_name);
+                    $em->persist($usuario);
+                    $em->flush();
+                
+                    // Devolvemos el Mensaje de Array
+                    $data = array(
+                        "status" => "success",
+                        "code" => 200,
+                        "msg" => "Imagen for user uploaded success !!"
+                    );
+                } else {
+                    // Devolvemos el Mensaje de Array, cuando la Imagen no sea valida
+                    $data = array(
+                        "status" => "error",
+                        "code" => 400,
+                        "msg" => "File not valid !!"
+                    );
+                }
+            }else{
+                $data = array(
+                    "status" => "error",
+                    "code" => 400,
+                    "msg" => "Imagen not upload !!"
+                );                
+            }            
+        }else{
+            $data = array(
+               "status" => "error",
+               "code" => 400,
+               "msg" => "Autorización no valida !!"
+            );            
+        }
+        //Retorno de la Funcion ************************************************
+        return $helpers->parserJson($data);
+    }
     
 }
