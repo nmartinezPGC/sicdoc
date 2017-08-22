9578b91d1fbd4465bf48b2539b9871992e06aa9a
nmartinezPGC <nahum.sreci@gmail.com>
2017-08-21 22:55:11 -0600

    NAM | Editar Documento
    Se Agrego la Accion de Editar el Documento, como parametro el Codigo del mismo y validando que solo el usuario que lo ingreso lo pueda Modifcar.

--- symfony/src/AppBundle/Controller/DocumentosController.php
+++ symfony/src/AppBundle/Controller/DocumentosController.php
@@ -77,9 +77,9 @@ class DocumentosController extends Controller{
                            "codUsuario" => $identity->codUser
                            // "idUsuario" => $identity->sub
                         ));                    
-                    $documentoNew->setCodUsuario($usuario); //Set de Codigo de Usuario
+                    $documentoNew->setIdUsuario($usuario); //Set de Codigo de Usuario
                     //Instanciamos de la Clase TblCorrespondenciaDet
-                                  echo "Voy por Aka 1.3  " . $identity->codUser;      
+                    //echo "Voy por Aka 1.3  " . $identity->codUser;      
                     $correspondencia = $em->getRepository("BackendBundle:TblCorrespondenciaDet")->findOneBy(                            
                         array(
                            "codCorrespondenciaDet" => $cod_correspondencia_det
@@ -149,5 +149,111 @@ class DocumentosController extends Controller{
         //Retorno de la Funcion ************************************************
         return $helpers->parserJson($data);
     } //Fin de la Funcion New Documento ****************************************
+    
+    //Funcion de Nuevo Documento *********************************************************************************************************
+    //************************************************************************************************************************************
+    public function editAction(Request $request, $id = null) {
+        //Instanciamos el Servicio Helpers
+        $helpers = $this->get("app.helpers");
+        //Recoger el Hash
+        //Recogemos el Hash y la Autrizacion del Mismo
+        $hash = $request->get("authorization", null);
+        //Se Chekea el Token
+        $checkToken = $helpers->authCheck($hash);
+        //Evalua que el Token sea True
+        if($checkToken == true){
+            $identity = $helpers->authCheck($hash, true);
+            
+            //Convertimos los Parametros POSt a Json
+            $json = $request->get("json", null);
+            
+            //Comprobamos que Json no es Null
+            if ($json != null) {
+                $params = json_decode($json);
+
+                //Parametros a Convertir                                
+                //Datos generales de la Tabla
+                $documentoId_2 = $id;
+                
+                //$cod_documento      = ($params->cod_documento != null) ? $params->cod_documento : null ;
+                $desc_documento     = ($params->desc_documento != null) ? $params->desc_documento : null ;
+                $url_documento      = ($params->url_documento != null) ? $params->url_documento : null ; 
+                $fecha_modificacion = new \DateTime('now');
+                $image              = null;
+                $status             = ($params->status != null) ? $params->status : null ;
+                //Relaciones de la Tabla con Otras
+                //$cod_correspondencia_det  = ($params->cod_correspondencia_det != null) ? $params->cod_correspondencia_det : null ;
+                //$cod_usuario          = ($params->cod_usuario != null) ? $params->cod_usuario : null ;
+                $cod_usuario        = $identity->sub;               
+                
+                //Evaluamos que el Codigo de Usuario no sea Null y la Descripcion tambien
+                if($cod_usuario != null && $desc_documento != null){
+                    //La condicion fue Exitosa
+                    //Instancia del Doctrine
+                    $em = $this->getDoctrine()->getManager();
+                    
+                    //Repositorio de la Tabla: TblUsuarios
+                    $documentoNew = $em->getRepository("BackendBundle:TblDocumentos")->findOneBy(
+                        array(
+                            "codDocumento" => $documentoId_2
+                        ));
+                    
+                        //Asignamos el usuario de la Consulta Anterior
+                        $idUsarioDocumento = $documentoNew->getIdUsuario();
+                    
+                        //Evaluamos que el Usuario de la Consulta del Token, sea el dueño
+                        // del Documento
+                        if(isset($identity->sub) && $identity->sub === $idUsarioDocumento->getIdUsuario()){
+                            //$documentoNew->setCodDocumento($cod_documento);
+                            $documentoNew->setDescDocumento($desc_documento);
+                            $documentoNew->setUrlDocumento($url_documento);
+                            $documentoNew->setFechaModificacion($fecha_modificacion);
+                            $documentoNew->setMiniImagen($image);
+                            $documentoNew->setStatus($status);
+
+                            //Realizar la Persistencia de los Datos y enviar a la BD
+                            $em->persist($documentoNew);
+                            $em->flush();
+
+                            //Array de Mensajes
+                            $data = array(
+                                "status" => "success", 
+                                "code"   => 200,
+                                "msg"   => "Documento actualizado, exitosamente !!"
+                            );
+                        } else {
+                            //Array de Mensajes
+                            $data = array(
+                                "status" => "error", 
+                                "code"   => 400, 
+                                "msg"   => "Documento no ha sido actualizado, no eres el creador del Documento !!"
+                            );
+                        }
+                    }else{
+                        $data = array(
+                            "status" => "error", 
+                            "code"   => 400, 
+                            "data"   => "Error al registrar, ya existe un documento con ese Codigo !!"
+                        );                       
+                    } //Finaliza el Bloque de la validadacion de la Data en la Tabla
+            } else {
+                    //Array de Mensajes
+                    $data = array(
+                       "status" => "error", 
+                       "code"   => 400, 
+                       "msg"   => "Documento no Creado, parametros invalidos !!"
+                    );
+                }
+            } else {
+            $data = array(
+                "status" => "error",                
+                "code" => "400",                
+                "msg" => "Autorizacion de Token no valida !!"                
+            );
+        }        
+        //Retorno de la Funcion ************************************************
+        return $helpers->parserJson($data);
+    } //Fin de la Funcion Edit Documento ****************************************
+      
     
 }
