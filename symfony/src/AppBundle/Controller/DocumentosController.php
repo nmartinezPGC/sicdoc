9266a720712ca57e17d46252bcb28956f0f01899
Nahum Martinez <nahum.sreci@gmail.com>
2017-08-22 12:15:18 -0600

    NAM | Servicio para Subir Imagenes o Pdf
    Se agrego, Servicio para subir las Imagenes o Pdf, a la BD, una ves que la Actividad (TblCorrespondenciaDet), se ingrese se llama a este metodo para Ingresar el Documento(s) que sean nesearios como soporte de esa Actividad y resolucion de la Comunicacion Entrante.

--- symfony/src/AppBundle/Controller/DocumentosController.php
+++ symfony/src/AppBundle/Controller/DocumentosController.php
@@ -12,15 +12,24 @@ use BackendBundle\Entity\TblUsuarios;
 use BackendBundle\Entity\TblDocumentos;
 use BackendBundle\Entity\TblCorrespondenciaDet;
 
-/**
- * Description of DocumentosController
- *
- * @author Nahum Martinez <nahum.sreci@gmail.com>
- */
+/*******************************************************************************
+ * Description of DocumentosController                                         * 
+ * Controlador para Subir Ficheros de los Documentos que se anexan a la        * 
+ * Documentacion de la Comunicacion entrante.                                  * 
+ * Llamado: Se debera ejecutar el Llamado a los servicios de esta Clase        * 
+ * una ves que se Ingrese el Detalle de Actividad en Respuesta                 * 
+ * @author Nahum Martinez <nahum.sreci@gmail.com>                              * 
+ * @category Ficheros                                                          * 
+ * @version 1.0                                                                * 
+ *******************************************************************************/
 class DocumentosController extends Controller{
-    //Accion de Prueba
-    //Funcion de Nuevo Documento *********************************************************************************************************
-    //************************************************************************************************************************************
+    
+    
+    /* Funcion de Nuevo Documento **********************************************
+     * Parametros:                                                             * 
+     * 1 ) Recibe un Objeto Request con el Metodo POST, el Json de la          *  
+     *     Informacion.                                                        * 
+     ***************************************************************************/
     public function newAction(Request $request) {
         //Instanciamos el Servicio Helpers
         $helpers = $this->get("app.helpers");
@@ -150,8 +159,13 @@ class DocumentosController extends Controller{
         return $helpers->parserJson($data);
     } //Fin de la Funcion New Documento ****************************************
     
-    //Funcion de Nuevo Documento *********************************************************************************************************
-    //************************************************************************************************************************************
+    
+    /* Funcion de Editar Documento *********************************************
+     * Parametros:                                                             * 
+     * 1 ) Recibe un Objeto Request con el Metodo POST, el Json de la          *  
+     *     Informacion.                                                        * 
+     * 2 ) Recibe el Codigo del Documento por medio de la Url.                 * 
+     ***************************************************************************/
     public function editAction(Request $request, $id = null) {
         //Instanciamos el Servicio Helpers
         $helpers = $this->get("app.helpers");
@@ -172,19 +186,18 @@ class DocumentosController extends Controller{
                 $params = json_decode($json);
 
                 //Parametros a Convertir                                
-                //Datos generales de la Tabla
-                $documentoId_2 = $id;
-                
-                //$cod_documento      = ($params->cod_documento != null) ? $params->cod_documento : null ;
+                //Parametro de la Url
+                $documentoId_2      = $id;
+                      
+                //Datos generales de la Tabla, que viene del Json
                 $desc_documento     = ($params->desc_documento != null) ? $params->desc_documento : null ;
                 $url_documento      = ($params->url_documento != null) ? $params->url_documento : null ; 
                 $fecha_modificacion = new \DateTime('now');
                 $image              = null;
                 $status             = ($params->status != null) ? $params->status : null ;
-                //Relaciones de la Tabla con Otras
-                //$cod_correspondencia_det  = ($params->cod_correspondencia_det != null) ? $params->cod_correspondencia_det : null ;
-                //$cod_usuario          = ($params->cod_usuario != null) ? $params->cod_usuario : null ;
-                $cod_usuario        = $identity->sub;               
+                
+                //Relaciones de la Tabla con Otras                
+                $cod_usuario        = $identity->sub;
                 
                 //Evaluamos que el Codigo de Usuario no sea Null y la Descripcion tambien
                 if($cod_usuario != null && $desc_documento != null){
@@ -196,38 +209,49 @@ class DocumentosController extends Controller{
                     $documentoNew = $em->getRepository("BackendBundle:TblDocumentos")->findOneBy(
                         array(
                             "codDocumento" => $documentoId_2
-                        ));
-                    
-                        //Asignamos el usuario de la Consulta Anterior
-                        $idUsarioDocumento = $documentoNew->getIdUsuario();
-                    
-                        //Evaluamos que el Usuario de la Consulta del Token, sea el dueño
-                        // del Documento
-                        if(isset($identity->sub) && $identity->sub === $idUsarioDocumento->getIdUsuario()){
-                            //$documentoNew->setCodDocumento($cod_documento);
-                            $documentoNew->setDescDocumento($desc_documento);
-                            $documentoNew->setUrlDocumento($url_documento);
-                            $documentoNew->setFechaModificacion($fecha_modificacion);
-                            $documentoNew->setMiniImagen($image);
-                            $documentoNew->setStatus($status);
+                        ));                   
+                        
+                        //Evaluo el Resultado del Query, con el Paramtro del Codigo del
+                        //Documento, para verificar si existe en la BD
+                        if(count($documentoNew) > 0){
+                            //Asignamos el usuario de la Consulta Anterior
+                            $idUsarioDocumento = $documentoNew->getIdUsuario()->getIdUsuario();
+                            
+                            //Evaluamos que el Usuario de la Consulta del Token, sea el dueño
+                            // del Documento
+                            if (isset($identity->sub) && $identity->sub === $idUsarioDocumento) {
+                                //$documentoNew->setCodDocumento($cod_documento);
+                                $documentoNew->setDescDocumento($desc_documento);
+                                $documentoNew->setUrlDocumento($url_documento);
+                                $documentoNew->setFechaModificacion($fecha_modificacion);
+                                $documentoNew->setMiniImagen($image);
+                                $documentoNew->setStatus($status);
 
-                            //Realizar la Persistencia de los Datos y enviar a la BD
-                            $em->persist($documentoNew);
-                            $em->flush();
+                                //Realizar la Persistencia de los Datos y enviar a la BD
+                                $em->persist($documentoNew);
+                                $em->flush();
 
-                            //Array de Mensajes
-                            $data = array(
-                                "status" => "success", 
-                                "code"   => 200,
-                                "msg"   => "Documento actualizado, exitosamente !!"
-                            );
-                        } else {
+                                //Array de Mensajes
+                                $data = array(
+                                    "status" => "success",
+                                    "code" => 200,
+                                    "msg" => "Documento actualizado, exitosamente !!"
+                                );
+                            } else {
+                                //Array de Mensajes
+                                $data = array(
+                                    "status" => "error",
+                                    "code" => 400,
+                                    "msg" => "Documento no ha sido actualizado, no eres el creador del Documento !!"
+                                );
+                            }
+                        }else{                            
                             //Array de Mensajes
                             $data = array(
                                 "status" => "error", 
-                                "code"   => 400, 
-                                "msg"   => "Documento no ha sido actualizado, no eres el creador del Documento !!"
-                            );
+                                "code"   => 400,
+                                "msg"   => "No existe un Documento con ese Codigo !!"
+                            );                           
                         }
                     }else{
                         $data = array(
@@ -241,7 +265,7 @@ class DocumentosController extends Controller{
                     $data = array(
                        "status" => "error", 
                        "code"   => 400, 
-                       "msg"   => "Documento no Creado, parametros invalidos !!"
+                       "msg"   => "Documento no actualizado, parametros invalidos !!"
                     );
                 }
             } else {
@@ -253,7 +277,107 @@ class DocumentosController extends Controller{
         }        
         //Retorno de la Funcion ************************************************
         return $helpers->parserJson($data);
-    } //Fin de la Funcion Edit Documento ****************************************
-      
+    } //Fin de la Funcion Editar Documento *************************
+    
+    
+    /* Funcion de Subir Documento **********************************************
+     * Parametros:                                                             * 
+     * 1 ) Recibe un Objeto Request con el Metodo POST, el Json de la          *  
+     *     Informacion.                                                        * 
+     * 2 ) Recibe el Codigo del Documento por medio de la Url.                 * 
+     ***************************************************************************/
+    public function uploadAction(Request $request, $id) {
+        //Instanciamos el Servicio Helpers
+        $helpers = $this->get("app.helpers");
+        //Recoger el Hash
+        //Recogemos el Hash y la Autrizacion del Mismo
+        $hash = $request->get("authorization", null);
+        //Se Chekea el Token
+        $checkToken = $helpers->authCheck($hash);
+        //Evalua que el Token sea True
+        if($checkToken == true){
+            $identity = $helpers->authCheck($hash, true);
+            
+            $documento_id = $id;
+            $em = $this->getDoctrine()->getManager(); 
+            
+            $documentoEM = $em->getRepository("BackendBundle:TblDocumentos")->findOneby(
+                array(
+                    "codDocumento" => $documento_id
+                ));                   
+            
+            //Evaluo el Resultado del Query, con el Paramtro del Codigo del
+            //Documento, para verificar si existe en la BD
+            if(count($documentoEM) > 0){
+                //Asignamos el usuario de la Consulta Anterior
+                $idUsarioDocumento = $documentoEM->getIdUsuario()->getIdUsuario();
+                
+                //Evaluamos que el Usuario de la Consulta del Token, sea el dueño
+                // del Documento
+                if(isset($identity->sub) && $identity->sub === $idUsarioDocumento) {
+                    //Recogemos los Archivos
+                    $file = $request->files->get('image', null);
+                    $file_pdf = $request->files->get('pdf', null);
+
+                    if ($file != null && !empty($file)) {
+                        $ext = $file->guessExtension();
+
+                        //Comprobamos la extencion sea Correcta
+                        if ($ext == "jpg" || $ext == "png" || $ext == "jpeg" || $ext == "pdf" ) {
+                            //Secuencia para Definir la Fecha y la Extencion del
+                            //Archivo a Subir
+                            //$file_name = time(). "." .$ext;                            
+                            $file_name = $documentoEM->getCodDocumento() . "-" . date('Y-m-d'). "." .$ext;                            
+                            $path_of_file = "uploads/correspondencia/correspondencia_".$documentoEM->getCodDocumento();
+                            
+                            $file->move($path_of_file, $file_name);
+                            
+                            //Steamos el valor de la Imagen a la BD
+                            $documentoEM->setUrlDocumento($file_name);
+
+                            $em->persist($documentoEM);
+                            $em->flush();
+
+                            //Array de Mensajes
+                            $data = array(
+                                "status" => "success",
+                                "code" => 200,
+                                "msg" => "Documento ha sido actualizado, la Imagen se ha cargado !!"
+                            );
+                        } else {
+                            //Array de Mensajes
+                            $data = array(
+                                "status" => "error",
+                                "code" => 400,
+                                "msg" => "El formato de imagen no es correcto !!"
+                            );
+                        }
+                    }
+                } else {
+                    //Array de Mensajes
+                    $data = array(
+                        "status" => "error",
+                        "code" => 400,
+                        "msg" => "Documento no ha sido actualizado, no eres el creador del Documento !!"
+                    );
+                }
+            }else{                            
+                //Array de Mensajes
+                $data = array(
+                    "status" => "error", 
+                    "code"   => 400,
+                    "msg"   => "No existe un Documento con ese Codigo !!"
+                );                           
+            }           
+        } else {
+            $data = array(
+                "status" => "error",                
+                "code" => "400",                
+                "msg" => "Autorizacion de Token no valida !!"                
+            );
+        }   
+        //Retorno de la Funcion ************************************************
+        return $helpers->parserJson($data);
+    } //Fin de la Funcion Subir Arhcivo **********************************
     
 }
