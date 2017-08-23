98743c070bece662b0f0810e627b506e6fe8bc95
Nahum Martinez <nahum.sreci@gmail.com>
2017-08-23 08:07:53 -0600

    NAM | Configuracion de Cabezeras PHP
    Se configuro los arhivos app_dev y app de Symfony, para que permita el Acceso a los Servicios de la API desde la APlicion SPA de Angular

--- symfony/web/app.php
+++ symfony/web/app.php
@@ -2,6 +2,16 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+//Configuracion de Cabezeras para que lo Utilize Angular
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER["REQUEST_METHOD"];
+if($method == "OPTIONS"){
+    die();   
+}
+
 /**
  * @var Composer\Autoload\ClassLoader
  */
