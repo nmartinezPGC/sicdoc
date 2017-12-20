4ab25d9d8320c809cb5a0925cda7198467ec5151
nmartinezPGC <nahum.sreci@gmail.com>
2017-12-19 22:00:15 -0600

    NAM
    Cambiar losParametros de la Localhost

--- Angular_SICDOC/sicdoc/src/app/services/seguimiento/agregar.actividad.service.ts
+++ Angular_SICDOC/sicdoc/src/app/services/seguimiento/agregar.actividad.service.ts
@@ -10,9 +10,10 @@ import { Observable } from 'rxjs/Observable';
 export class AgregarActividadService {
   //Propiedades de la Clases
   //URL Base de la Clase, Referencia a la API | Symfony
-  public url = "http://localhost/sicdoc/symfony/web/app_dev.php";
+  // public url = "http://localhost/sicdoc/symfony/web/app_dev.php";
   // public url = "http://172.17.4.162/sicdoc/symfony/web/app.php";
   // public url = "http://172.17.3.141/sicdoc/symfony/web/app.php";
+  public url = "http://192.168.0.23/sicdoc/symfony/web/app.php";
 
   //Variables para el localStorage
   public identity;
